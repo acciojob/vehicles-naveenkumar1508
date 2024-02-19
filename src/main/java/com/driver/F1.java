b2923a4d39649a7a1d7bd9e65e208baec0ee80c0
Naveenkumar <naveent1508@gmail.com>
2024-02-19 15:55:55 +0530

    Vehcile Assignment

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -2,29 +2,76 @@ package com.driver;
 
 public class F1 extends Car {
 
+    private boolean isManual;
     public F1(String name, boolean isManual) {
         //Use arbitrary values for parameters which are not mentioned
+        super(name,4,2,6,isManual,"Race car",1);
+        this.isManual = isManual;
     }
 
-    public void accelerate(int rate){
-        int newSpeed = 0; //set the value of new speed by using currentSpeed and rate
-        /**
-         * speed 0: gear 1
-         * speed 1-50: gear 1
-         * speed 51-100: gear 2
-         * speed 101-150: gear 3
-         * speed 151-200: gear 4
-         * speed 201-250: gear 5
-         * speed more than 250: gear 6
-         */
+    // Getters for F1 class
+    @Override
+    public String getName() {
+        return super.getName();
+    }
 
-        if(newSpeed == 0) {
-            //Stop the car, set gear as 1
-        }
-        //for all other cases, change the gear accordingly
+    public boolean isManual() {
+        return isManual;
+    }
 
-        if(newSpeed > 0) {
-            changeSpeed(newSpeed, getCurrentDirection());
+    public int getCurrentGear() {
+        return super.getCurrentGear();
+    }
+
+//    public void accelerate(int rate){
+//
+//        int newSpeed = 0; //set the value of new speed by using currentSpeed and rate
+//        /**
+//         * speed 0: gear 1
+//         * speed 1-50: gear 1
+//         * speed 51-100: gear 2
+//         * speed 101-150: gear 3
+//         * speed 151-200: gear 4
+//         * speed 201-250: gear 5
+//         * speed more than 250: gear 6
+//         */
+//
+//        if(newSpeed == 0) {
+//            //Stop the car, set gear as 1
+//        }
+//        //for all other cases, change the gear accordingly
+//
+//        if(newSpeed > 0) {
+//            changeSpeed(newSpeed, getCurrentDirection());
+//        }
+//    }
+public void accelerate(int rate) {
+    int newSpeed = getCurrentSpeed() + rate;
+    int newGear = determineGear(newSpeed);
+
+    if (newSpeed <= 0) {
+        stop();
+    } else {
+        changeGear(newGear);
+        changeSpeed(newSpeed, getCurrentDirection());
+    }
+}
+
+    private int determineGear(int speed) {
+        if (speed <= 0) {
+            return 1;
+        } else if (speed <= 50) {
+            return 1;
+        } else if (speed <= 100) {
+            return 2;
+        } else if (speed <= 150) {
+            return 3;
+        } else if (speed <= 200) {
+            return 4;
+        } else if (speed <= 250) {
+            return 5;
+        } else {
+            return 6;
         }
     }
 }
