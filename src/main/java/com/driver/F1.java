72c2246ec723c0164807ef5452d7c7cd10ee9118
Naveenkumar <naveent1508@gmail.com>
2024-02-19 17:11:34 +0530

    changes Done

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -2,11 +2,11 @@ package com.driver;
 
 public class F1 extends Car {
 
-    private boolean isManual;
+
     public F1(String name, boolean isManual) {
         //Use arbitrary values for parameters which are not mentioned
-        super(name,4,2,6,isManual,"Race car",1);
-        this.isManual = isManual;
+        super("mercedes",4,2,6,false,"Race car",1);
+
     }
 
     // Getters for F1 class
@@ -15,11 +15,8 @@ public class F1 extends Car {
         return super.getName();
     }
 
-    public boolean isManual() {
-        return isManual;
-    }
-
     public int getCurrentGear() {
+
         return super.getCurrentGear();
     }
 
